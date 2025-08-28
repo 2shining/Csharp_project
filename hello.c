ebb1d494964838f2828a13f3d06b68a493e67b34
Fetra Faneva <114089063+2shining@users.noreply.github.com>
2025-08-28 09:01:17 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("1174 H-F RASAMIMANANA Fetra Faneva\n"
+           "1203 H-F RAMARIARISON Andritiana Henriono\n"
+           "1304 H-F RABEARINJATOVO Stevyne Ny aina Samuel Patricino\n"
+           "1209 H-F ANDRIAHERILANTO Antsa Fiderana\n");
+    return 0;
+}
